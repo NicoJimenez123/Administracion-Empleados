12528373f81bb43631025aaeef6037341e7ad2f6
MauroGerman <63725082+maurogerman@users.noreply.github.com>
2020-06-01 18:17:30 -0300

    Agregados metdos en Empresa y clase Main p/test
    EL metodo borrar quita de la lista un Trabajador pasado por parámetro,
    El método mostrar muestra los nombres de la lista, es útil para vhequear el funcionamiento.
    En la clase Main hice las pruebas de los métodos agregados

--- src/Empresa.java
+++ src/Empresa.java
@@ -1,93 +1,140 @@
+
 import ayp3.tp.*;
+
+
 public class Empresa implements IEmpresa{
-	NodoTrabajador primero;
-	NodoTrabajador ultimo;
-	public Empresa() {
-		this.primero = null;
-	}
-	@Override
-	public void agregarEmpleado(ITrabajador trabajador) {
-		// Primero creo un nodo con el trabajador a insertar
-		NodoTrabajador nuevoTrabajador = new NodoTrabajador(trabajador);
-		// Me fijo si hay algun trabajador en la lista
-		if(this.primero == null) {
-			// Si no lo hay, tomo al nuevo trabajador y lo seteo como primero y ultimo en la lista
-			this.primero = nuevoTrabajador;
-			this.ultimo = nuevoTrabajador;
-		}
-		else {
-			// Enlazo al nuevo trabajador con el ultimo en la lista
-			this.ultimo.setSiguiente(nuevoTrabajador);
-			// Tambien enlazo al ultimo de la lista con el nuevo
-			nuevoTrabajador.setAnterior(this.ultimo);
-			// Ahora el nuevo pasa a ser el ultimo
-			this.ultimo = nuevoTrabajador;
-		}
-	}
-	@Override
-	public void agregarDirectivo(ITrabajador trabajador) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void quitarEmpleado(ITrabajador trabajador) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void quitarDirectivo(ITrabajador trabajador) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void quitarEmpleado(long dni) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void quitarDirectivo(long dni) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public ITrabajador obtenerTrabajador(long dni) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public void liquidarSueldos() {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public double obtenerMontoTotalAPagar() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public ITrabajador obtenerTrabajadorSalarioMinimo() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public ITrabajador obtenerTrabajadorSalarioMaximo() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public double obtenerPromedioSalarios() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public ITrabajador obtenerTrabajadorMasAntiguo() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public ITrabajador obtenerTrabajadorMasReciente() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
+    
+    NodoTrabajador primero;
+    NodoTrabajador ultimo;
+    
+    
+    public Empresa() {
+            this.primero = null;
+    }
+    
+    
+    @Override
+    public void agregarEmpleado(ITrabajador trabajador) {
+            // Primero creo un nodo con el trabajador a insertar
+            NodoTrabajador nuevoTrabajador = new NodoTrabajador(trabajador);
+            // Me fijo si hay algun trabajador en la lista
+            if(this.primero == null) {
+                    // Si no lo hay, tomo al nuevo trabajador y lo seteo como primero y ultimo en la lista
+                    this.primero = nuevoTrabajador;
+                    this.ultimo = nuevoTrabajador;
+            }
+            else {
+                    // Enlazo al nuevo trabajador con el ultimo en la lista
+                    this.ultimo.setSiguiente(nuevoTrabajador);
+                    // Tambien enlazo al ultimo de la lista con el nuevo
+                    nuevoTrabajador.setAnterior(this.ultimo);
+                    // Ahora el nuevo pasa a ser el ultimo
+                    this.ultimo = nuevoTrabajador;
+            }
+    }
+    
+    
+    
+    
+    
+
+    @Override
+    public void agregarDirectivo(ITrabajador trabajador) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void quitarEmpleado(ITrabajador trabajador) {
+        //verifico si la lista-empresa esta vacía
+        if(this.primero==null){/*tirar exepcion*/}
+        
+        //creo los nodos que me serviran en la iteración
+        NodoTrabajador este= this.primero;  
+    	NodoTrabajador previo= null;  
+
+    	while(este!=null){
+            if(este.persona.equals(trabajador)){  //si el nodo "tocado" contiene al trabajador buscado
+                if(previo==null){   
+                    this.primero = this.primero.getSiguiente();    
+                    este.setSiguiente(null);    
+                    este=this.primero;  
+                }else{
+                    previo.setSiguiente(este.getSiguiente());
+                    este.setSiguiente(null);
+                    este= previo.getSiguiente();
+                }                
+            }else{
+                previo=este;
+                este=este.getSiguiente();
+            }
+        }
+    }
+
+    @Override
+    public void quitarDirectivo(ITrabajador trabajador) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void quitarEmpleado(long dni) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void quitarDirectivo(long dni) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ITrabajador obtenerTrabajador(long dni) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void liquidarSueldos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double obtenerMontoTotalAPagar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ITrabajador obtenerTrabajadorSalarioMinimo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ITrabajador obtenerTrabajadorSalarioMaximo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double obtenerPromedioSalarios() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ITrabajador obtenerTrabajadorMasAntiguo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ITrabajador obtenerTrabajadorMasReciente() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    //METODOS ÚTILES, FUERA DE LOS PEDIDOS POR LA CONSIGNA
+    public void mostrarNombres(){
+        NodoTrabajador n = this.primero;
+
+    	while(n != null){
+    		System.out.println(n.persona.getNombre()+", ");
+    		n=n.siguiente;
+    	}
+    }
+    
+    
+    
 }
