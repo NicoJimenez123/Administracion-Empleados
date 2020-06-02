57d6aa09571089d281ad5ba7c698a03cdfb38a5d
MauroGerman <63725082+maurogerman@users.noreply.github.com>
2020-06-02 10:48:09 -0300

    Upload Empresa, falta testear
    Listos casi todos los metodos de la clase Empresa, falta testear y coordinar con la clase empleados de Debi

--- src/Empresa.java
+++ src/Empresa.java
@@ -1,5 +1,7 @@
+import ayp3.tp.ITrabajador;
+import ayp3.tp.TipoCargo;
 
-import ayp3.tp.*;
+
 
 
 public class Empresa implements IEmpresa{
@@ -77,7 +79,29 @@ public class Empresa implements IEmpresa{
 
     @Override
     public void quitarEmpleado(long dni) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //verifico si la lista-empresa esta vacía
+        if(this.primero==null){/*tirar exepcion*/}
+        
+        //creo los nodos que me serviran en la iteración
+        NodoTrabajador este= this.primero;  
+    	NodoTrabajador previo= null;  
+
+    	while(este!=null){
+            if(este.persona.getDni()==(dni)){  //si el nodo "tocado" contiene al dni buscado
+                if(previo==null){   
+                    this.primero = this.primero.getSiguiente();    
+                    este.setSiguiente(null);    
+                    este=this.primero;  
+                }else{
+                    previo.setSiguiente(este.getSiguiente());
+                    este.setSiguiente(null);
+                    este= previo.getSiguiente();
+                }                
+            }else{
+                previo=este;
+                este=este.getSiguiente();
+            }
+        }
     }
 
     @Override
@@ -87,52 +111,168 @@ public class Empresa implements IEmpresa{
 
     @Override
     public ITrabajador obtenerTrabajador(long dni) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ITrabajador trabajador = new Trabajador();
+        
+        //recorro la pila hasta obtener el trabajador con el DNI buscado
+        NodoTrabajador n = this.primero;
+        while(n!=null){
+            if(n.persona.getDni()==dni){
+                trabajador=n.persona;
+            }
+            
+            n=n.siguiente;
+        }
+        
+        return trabajador;
     }
 
     @Override
     public void liquidarSueldos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //recorro la lista-empresa y seteo el atributo sueldo de cada trabajador 
+        ITrabajador trabajador = new Trabajador();        
+      
+        NodoTrabajador n = this.primero;
+        while(n!=null){
+            n.persona.setSalario(n.persona.getSalario() + n.persona.getMontoACobrar());
+            
+            n=n.siguiente;
+        }                
     }
 
     @Override
     public double obtenerMontoTotalAPagar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //recorro la lista-empresa y sumo los montos a pagar
+        double contador=0;
+        
+        ITrabajador trabajador = new Trabajador();        
+      
+        NodoTrabajador n = this.primero;
+        while(n!=null){
+            contador += n.persona.getMontoACobrar();
+            
+            n=n.siguiente;
+        }  
+        
+        return contador;
     }
 
     @Override
     public ITrabajador obtenerTrabajadorSalarioMinimo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        //recorro la lista-empresa chequeo cada salario
+        double minimo=100000;   //uso el maximo valor de salario que se puede ganar, como referencia
+        ITrabajador trabajadorMinimo = new Trabajador(); //guarda al trabajador con salario minimo
+        
+        ITrabajador trabajador = new Trabajador();   //para la iteración      
+      
+        NodoTrabajador n = this.primero;
+        while(n!=null){
+            if(n.persona.getSalario() < minimo){
+                minimo = n.persona.getSalario();
+                trabajadorMinimo = n.persona;                
+            }
+            
+            n=n.siguiente;
+        }  
+        
+        return trabajadorMinimo;
     }
 
     @Override
     public ITrabajador obtenerTrabajadorSalarioMaximo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //recorro la lista-empresa chequeo cada salario
+        double maximo=0;   
+        ITrabajador trabajadorMaximo = new Trabajador(); //guarda al trabajador con salario máximo
+        
+        ITrabajador trabajador = new Trabajador();   //para la iteración      
+      
+        NodoTrabajador n = this.primero;
+        while(n!=null){
+            if(n.persona.getSalario() >= maximo){
+                maximo = n.persona.getSalario();
+                trabajadorMaximo = n.persona;                
+            }
+            n=n.siguiente;
+        }  
+        
+        return trabajadorMaximo;
     }
 
     @Override
     public double obtenerPromedioSalarios() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //recorro la lista-empresa chequeo cada salario
+        double sumatoria=0;   
+        ITrabajador trabajador = new Trabajador();   //para la iteración      
+      
+        NodoTrabajador n = this.primero;
+        while(n!=null){
+            sumatoria += n.persona.getSalario();
+            n=n.siguiente;
+        }  
+        
+        return sumatoria / this.longitud();
     }
 
     @Override
     public ITrabajador obtenerTrabajadorMasAntiguo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //recorro la lista-empresa chequeo cada antiguedad
+        double maximo=0;   
+        ITrabajador trabajadorMaximo = new Trabajador(); //guarda al trabajador con mayor antiguedad
+        
+        ITrabajador trabajador = new Trabajador();   //para la iteración      
+      
+        NodoTrabajador n = this.primero;
+        while(n!=null){
+            if(n.persona.getMesesAntiguedad() >= maximo){
+                maximo = n.persona.getMesesAntiguedad();
+                trabajadorMaximo = n.persona;                
+            }
+            n=n.siguiente;
+        }  
+        
+        return trabajadorMaximo;
     }
 
     @Override
     public ITrabajador obtenerTrabajadorMasReciente() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //recorro la lista-empresa chequeo cada antiguedad
+        double maximo=1000000;   
+        ITrabajador trabajadorMinimo = new Trabajador(); //guarda al trabajador con menor antiguedad
+        
+        ITrabajador trabajador = new Trabajador();   //para la iteración      
+      
+        NodoTrabajador n = this.primero;
+        while(n!=null){
+            if(n.persona.getMesesAntiguedad() <= maximo){
+                maximo = n.persona.getMesesAntiguedad();
+                trabajadorMinimo = n.persona;                
+            }
+            n=n.siguiente;
+        }  
+        
+        return trabajadorMinimo;
     }
     
     //METODOS ÚTILES, FUERA DE LOS PEDIDOS POR LA CONSIGNA
-    public void mostrarNombres(){
+    public void mostrarNombres(){ //muestra los nombres de los trabjadores dentro de la lista
         NodoTrabajador n = this.primero;
 
     	while(n != null){
     		System.out.println(n.persona.getNombre()+", ");
     		n=n.siguiente;
     	}
+    }
+    
+    public int longitud(){  //calcula la cantidad de nodos de la lista
+        int contador=0;
+        NodoTrabajador n = this.primero;
+
+    	while(n != null){
+    		contador++;
+                n=n.siguiente;
+    	}        
+        
+        return contador;
     }
     
     
