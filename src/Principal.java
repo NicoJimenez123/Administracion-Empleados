6f832073aa0573901c804d7aba2b7796aa64837c
NicoJimenez123 <nicolasdanieljimenez@hotmail.com>
2020-06-03 16:26:39 -0300

    Actualizado el metodo altaTrabajador()

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,7 @@ import ayp3.tp.TipoCargo;
 import unpaz.ayp3.Consola;
 
 public class Principal {
+	static Empresa empresa = new Empresa();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Fecha f=new Fecha(12,3,2015);
@@ -17,7 +18,7 @@ public class Principal {
 		while(true) {
 			ITrabajador t;
 			System.out.println("~~ Menu de Alta de Trabajadores ~~");
-			System.out.println();
+			System.out.println("Opcion 1) Dar de Alta de un Trabajador.");
 			int opcion = Consola.pedirEntero("Ingrese una Opcion: ");
 			switch(opcion) {
 			case 1:t= altaTrabajador();System.out.println(t.toString());break;
@@ -26,12 +27,13 @@ public class Principal {
 		}
 	}
 	public static ITrabajador altaTrabajador() {
+		// Primero voy declarando las variables para usar el constructor de Trabajador
 		long dni;
 		int dia, mes, anio, opcionListaCargo = -1;
 		String nombre, apellido, titulo, tituloPostgrado;
 		TipoCargo cargo = null;
 		Fecha fechaIngreso;
-		
+		// Y voy rellenando las variables usando bloques try-
 		while(true) {
 			try {
 				dni = Consola.pedirEntero("Ingrese el Numero de DNI: ");
@@ -41,59 +43,30 @@ public class Principal {
 				System.out.println("Por favor, Ingrese el Numero de DNI correspondiente.");
 			}
 		}
+		nombre = Consola.pedirTexto("Ingrese el Nombre: ");
+		apellido = Consola.pedirTexto("Ingrese el Apellido: ");
 		
-		while(true) {
-			try {
-				nombre = Consola.pedirTexto("Ingrese el Nombre: ");
-				break;
-			}
-			catch(Exception e) {
-				System.out.println("Por favor, Ingrese el Nombre correctamente");
-			}
-		}
-		
-		while(true) {
-			try {
-				apellido = Consola.pedirTexto("Ingrese el Apellido: ");
-				break;
-			}
-			catch(Exception e) {
-				System.out.println("Por favor, Ingrese el Apellido correctamente");
-			}
-		}
-		
-		TipoCargo[] listaDeCargos = listarCargos();
+		// Acá creo una lista con los diferentes tipos de cargos
+		TipoCargo[] listaDeCargos = TipoCargo.values();
 		do{
 			try {
+				// Al estar listados solo hay que ingresar el numero correspondiente a cada cargo.
 				opcionListaCargo = Consola.pedirEntero("Ingrese el Numero correspondiente al Cargo: ");
+				// Si no hay una excepcion, Obtengo el cargo dependiendo del numero ingresado.
 				cargo = listaDeCargos[opcionListaCargo];
 			}
 			catch(Exception e) {
 				System.out.println("Por favor, Ingrese una Opcion valida.");
 			}
+		// Este bloque se ejecutara mientras se ingresen valores menores a 0 y mayores a la cantidad de cargos
 		}while(opcionListaCargo < 0 && opcionListaCargo > TipoCargo.values().length);
 		
 		do {
+			// Bloque para ingresar la fecha
 			while(true) {
 				try {
 					dia = Consola.pedirEntero("Ingrese el dia de ingreso: ");
-					break;
-				}
-				catch(Exception e) {
-					System.out.println("Ha ocurrido un error, Ingrese el numero correspondiente.");
-				}
-			}
-			while(true) {
-				try {
 					mes = Consola.pedirEntero("Ingrese el mes de ingreso: ");
-					break;
-				}
-				catch(Exception e) {
-					System.out.println("Ha ocurrido un error, Ingrese el numero correspondiente.");
-				}
-			}
-			while(true) {
-				try {
 					anio = Consola.pedirEntero("Ingrese el anio de ingreso: ");
 					break;
 				}
@@ -101,24 +74,36 @@ public class Principal {
 					System.out.println("Ha ocurrido un error, Ingrese el numero correspondiente.");
 				}
 			}
+			// Creo un obj tipo Fecha con los valores ingresados
 			fechaIngreso = new Fecha(dia,mes,anio);
 			// Tirar mensaje de error si la fecha no es valida
 			if(!fechaIngreso.comprobar_fecha()) {
 				System.out.println("La Fecha ingresada no es valida");
 			}
+		// Ejecuto este bloque hasta que se ingrese una fecha valida
 		}while(!fechaIngreso.comprobar_fecha());
+		// Ingreso los titulos del trabajador
 		titulo = Consola.pedirTexto("Ingrese el Titulo Universitario: ");
 		tituloPostgrado = Consola.pedirTexto("Ingrese el Titulo de Postgrado: ");
-		System.out.println(cargo);
-		return new Trabajador(dni,nombre,apellido,cargo,fechaIngreso,titulo,tituloPostgrado);
+		// Creo un nuevo trabajador
+		ITrabajador trabajador = new Trabajador(dni,nombre,apellido,cargo,fechaIngreso,titulo,tituloPostgrado);
+		// Retorno la instancia del nuevo trabajador
+		return trabajador;
 	}
-
-    public static TipoCargo[] listarCargos() {
+	
+    public static void listarCargos() {
     	System.out.println("Lista de Cargos: ");
     	TipoCargo[] t = TipoCargo.values();
     	for(int i = 0; i < TipoCargo.values().length; i++) {
     		System.out.println(i + ": " + t[i]);
     	}
-    	return t;
+    }
+    
+    public static void mostrarPorTipo() {
+    	
     }
 }
+
+
+
+
