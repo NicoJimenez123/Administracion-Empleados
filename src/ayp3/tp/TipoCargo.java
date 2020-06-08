d7d545d406a2223c6e3da57e816d05fa9f6f195b
NicoJimenez123 <nicolasdanieljimenez@hotmail.com>
2020-06-08 19:37:32 -0300

    Añadido valores en los cargos

--- src/ayp3/tp/TipoCargo.java
+++ src/ayp3/tp/TipoCargo.java
@@ -2,16 +2,18 @@ package ayp3.tp;
 
 public enum TipoCargo {
 	
-	DIRECTOR_GENERAL("Director_general",70000),
-	DIRECTOR_DEPARTAMENTO("Director_departamento",50000), 
-	JEFES("Jefes",40000),
-	SUPERVISOR("Supervisor",30000),
-	OPERARIO("Operario",25000);
+	DIRECTOR_GENERAL("Director_general",70000,0),
+	DIRECTOR_DEPARTAMENTO("Director_departamento",50000,1), 
+	JEFES("Jefes",40000,2),
+	SUPERVISOR("Supervisor",30000,3),
+	OPERARIO("Operario",25000,4);
 	private String cargo;
 	private double salario;
-	TipoCargo(String cargo,double salario) {
+	private int valor;
+	TipoCargo(String cargo,double salario,int valor) {
 	        this.cargo= cargo;
 	        this.salario=salario;
+	        this.valor = valor;
 	        
 	    }//constructor
 
@@ -25,4 +27,15 @@ public enum TipoCargo {
 	    
 			return salario;
 	    }
+	    public int getValor() {
+	    	return this.valor;
+	    }
+		public boolean esMayor(TipoCargo cargo2) {
+			if(this.valor < cargo2.getValor()) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
 }
